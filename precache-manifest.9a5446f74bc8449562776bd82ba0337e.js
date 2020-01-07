7e2966358c6d1f96a769754a87405da1920969ae
Alam <alamkundan1300@gmail.com>
2020-01-07 18:50:36 +0530

    Updates

---
+++ precache-manifest.9a5446f74bc8449562776bd82ba0337e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b361ef3383046a88d7941a29e28dd838",
+    "url": "/FlickPicker/index.html"
+  },
+  {
+    "revision": "b900fde2e3d5ea787f64",
+    "url": "/FlickPicker/static/css/main.4698b205.chunk.css"
+  },
+  {
+    "revision": "68f656c2da7613929b3e",
+    "url": "/FlickPicker/static/js/2.6f5ff138.chunk.js"
+  },
+  {
+    "revision": "a8e8f54498cf9733db882a30e9908e7f",
+    "url": "/FlickPicker/static/js/2.6f5ff138.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b900fde2e3d5ea787f64",
+    "url": "/FlickPicker/static/js/main.193b8ac2.chunk.js"
+  },
+  {
+    "revision": "f5da69a1b20de30a2ff7",
+    "url": "/FlickPicker/static/js/runtime-main.168b64dd.js"
+  }
+]);
