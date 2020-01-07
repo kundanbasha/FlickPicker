77e0f221452530893f43608bed480978dc13d4b2
Alam <alamkundan1300@gmail.com>
2020-01-07 18:23:15 +0530

    basic version is ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import "./App.css";
+
+import configureStore from "./redux/configureStore";
+import MovieDetails from "./containers/MovieDetails";
+import NoPage from "./components/NoPage";
+import Movies from "./containers/Movies";
+
+const store = configureStore();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route path={"/"} component={Movies} exact={true} />
+          <Route path={"/movies"} component={Movies} exact={true} />
+          <Route path={"/movies/:id"} component={MovieDetails} />
+          <Route path={"*"} component={NoPage} />
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
